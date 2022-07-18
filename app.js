c030a98df1a496d22375bee9af4ac8e60e57789a
GustavoFFLima <frazao39@gmail.com>
2022-07-18 10:35:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// let button = document.getElementById('button');
+// let input1 = document.getElementById('input1');
+// let input2 = document.getElementById('input2');
+
+// function somarNumeros (input1, input2) {
+//     return Number(input1) + Number(input2)
+// }
+
+// button.addEventListener('click', () => {
+//     console.log(somarNumeros(input1.value, input2.value))
+// });
